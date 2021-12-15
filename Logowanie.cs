05d36cea6e8954d44f019eaa61c06ee0dc7d3489
grabj <justyna.grabowska43@gmail.com>
2021-12-15 17:12:59 +0100

    Dodaj pliki projektów.

---
+++ Logowanie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt
+{
+    public partial class Logowanie : Form
+    {
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 okno = new Form1();
+            this.Hide();
+            okno.Show();
+        }
+    }
+}
